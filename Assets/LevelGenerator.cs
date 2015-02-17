4f5b417a93a4afc7b108f263f1511fee9a690270
Rustin Wollin <rwollin96@gmail.com>
2015-02-17 01:08:54 -0500

    initial code/assets commit

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelGenerator : MonoBehaviour {
+	public Transform spawnTile; // preset tile to spawn in 0,0 as the player's spawn point
+	public List<Transform> tileList; // list of tiles to pick from during gen
+	public int levelWidth; // number of tiles the level is wide
+	public int levelHeight; // number of tiles the level is high
+
+	void Start () {
+		Transform currentTile;
+		int randomIndex;
+		for(int x = 0; x < levelWidth; x++) {
+			for(int y = 0; y < levelHeight; y++) {
+				if(x == 0 && y == 0) {
+					currentTile = Instantiate(spawnTile) as Transform;
+				} else {
+					randomIndex = Random.Range(0, tileList.Count);
+					currentTile = Instantiate(tileList[randomIndex]) as Transform;
+				}
+
+				currentTile.localPosition = new Vector3(currentTile.localPosition.x * (x*2 + 1),
+				                                        currentTile.localPosition.y * (y*2 + 1),
+				                                        currentTile.localPosition.z);
+				currentTile.parent = gameObject.transform;
+			}
+		}
+	}
+
+	void Update () {
+	
+	}
+}
---
+++ Assets/Player/PlayerInput.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+	public int maxHP = 100;
+	public float walkSpeed = 3f;
+	public float runSpeed = 8f;
+	
+	private float HP;
+	private Vector3 velocity = new Vector3(0f, 0f, 0f);
+	private Animator animator;
+	
+	void Start() {
+		animator = gameObject.GetComponent<Animator>();
+		walkSpeed /= 100; // divide by pixels per unit
+		runSpeed /= 100;  // to keep things from getting cuhrayzee
+	}
+
+	void Update() {
+		Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		input = Vector2.ClampMagnitude(input, 1);
+
+		if (Input.GetAxis("Run") > 0) {
+			velocity.x = input.x * runSpeed;
+			velocity.y = input.y * runSpeed;
+		} else {
+			velocity.x = input.x * walkSpeed;
+			velocity.y = input.y * walkSpeed;
+		}
+
+		/* sets the direction we're walking for the state machine
+		 * 0 left
+		 * 1 up
+		 * 2 right
+		 * 3 down
+		 */
+		if (velocity.x - Mathf.Abs(velocity.y) > 0) {
+			animator.SetInteger("WalkingDir", 2);
+		} else if (velocity.x + Mathf.Abs(velocity.y) < 0) {
+			animator.SetInteger("WalkingDir", 0);
+		} else if (velocity.y < 0) {
+			animator.SetInteger("WalkingDir", 3);
+		} else if (velocity.y > 0) {
+			animator.SetInteger("WalkingDir", 1);
+		}
+
+		animator.SetBool("IsMoving", (velocity.x != 0)||(velocity.y != 0));
+
+		//rigidbody2D.velocity += velocity;
+	}
+
+	void FixedUpdate() {
+		transform.position += velocity;
+	}
+}
